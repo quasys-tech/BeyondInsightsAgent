cec50fcdfb1c0d3187a9cf09d98d0eb87c2e3adf
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2024-04-01 15:00:52 +0300

    Update getsecrets.py

--- get_secrets.py
+++ get_secrets.py
@@ -5,12 +5,6 @@ from beyondtrust_agent import controller
 import logging
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
-os.environ["SECRETS_PATH"] = "/Users/abdulmelikkalkan/tmp/beyond/secrets_files"
-os.environ["BT_API_URL"] = "https://44.207.168.98:443/BeyondTrust/api/public/v3"
-os.environ["BT_API_KEY"] = "c79488314430a91e477d32bad64278c007f3b2860712785d25082a8a430e3325b99fa2be60f7c0cb4c5bd289d5e591cf211d0bc235d1776390bb286dd7b69933;runas=abdulmelik;"
-os.environ["FOLDER_LIST"] = "abdulmelik/Dev/"
-os.environ["BT_VERIFY_CA"] = "False"
-
 
 def main():
 
@@ -22,7 +16,7 @@ def main():
     secrets_json = json.loads(secrets)
     # for k, v in secrets_json.items():
     #     print(k, v)
-    # print(secrets_json["Quasys"]["beyondtrust"]["Password"])
+    print(secrets_json["Quasys"]["beyondtrust"]["Password"])
     server_object = HTTPServer(server_address=('', 8080), RequestHandlerClass=CGIHTTPRequestHandler)
     server_object.serve_forever()
 
